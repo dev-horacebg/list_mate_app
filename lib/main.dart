96ab80e7c9f684825754a9a994eaa79f06fb1484
HoraceBG <horaceb1986@gmail.com>
2019-03-07 21:50:16 +0000

    Further mini. Structuring storage solN.

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(ctx) => MaterialApp(home: Home());
+  build(ctx) => MaterialApp(home: Home());
 }
 
 class Home extends StatefulWidget {
@@ -19,62 +19,55 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   @override
-  Widget build(ctx) {
-    return FutureBuilder(
-        future: rootBundle.loadString('assets/data.json'),
-        builder: (context, snp) {
-          if (snp.connectionState == ConnectionState.done) {
-            var model = ListModel();
-            model.init(Menu.fromJson(json.decode(snp.data)));
-            return ScopedModel<ListModel>(
+  build(ctx) => FutureBuilder(
+      future: rootBundle.loadString('assets/data.json'),
+      builder: (context, snp) {
+        if (snp.connectionState == ConnectionState.done) {
+          var model = ListModel();
+          model.init(Menu.fromJson(json.decode(snp.data)));
+          return ScopedModel<ListModel>(
               model: model,
               child: Scaffold(
-                body: Stack(
-                  alignment: AlignmentDirectional.center,
-                  children:[
-                    Row(
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: ItemColumn(Dir.left),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: ItemColumn(Dir.right),
-                        ),
-                      ],
+                  body:
+                      Stack(alignment: AlignmentDirectional.center, children: [
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: ItemColumn(Dir.left),
                     ),
-                    ScopedModelDescendant<ListModel>(
-                      builder: (_, c, m) {
-                        return Draggable(
-                            onDragEnd: (end) {
-                              m.resetLists();
-                            },
-                            child: FloatingActionButton(
-                                onPressed: () {},
-                                backgroundColor: Colors.deepOrange),
-                            feedback: FloatingActionButton(
-                                onPressed: () {},
-                                backgroundColor: Colors.white),
-                            childWhenDragging: Opacity(opacity: 0));
-                      }
-                    )
-                  ]
-                )
-              )
-            );
-          } else {
-            return Container(color: Colors.blue);
-          }});
-  }
+                    Expanded(
+                      flex: 1,
+                      child: ItemColumn(Dir.right),
+                    ),
+                  ],
+                ),
+                ScopedModelDescendant<ListModel>(builder: (_, c, m) {
+                  return Draggable(
+                      onDragEnd: (end) {
+                        m.resetLists();
+                      },
+                      child: FloatingActionButton(
+                          onPressed: () {}, backgroundColor: Colors.deepOrange),
+                      feedback: FloatingActionButton(
+                          onPressed: () {}, backgroundColor: Colors.white),
+                      childWhenDragging: Opacity(opacity: 0));
+                })
+              ])));
+        } else {
+          return Container(color: Colors.blue);
+        }
+      });
 }
 
 class ItemWgt extends StatelessWidget {
-  final Item i; final Dir dir;
+  final Item i;
+  final Dir dir;
+
   ItemWgt(this.i, this.dir);
 
   @override
-  Widget build(ctx) => Expanded(
+  build(ctx) => Expanded(
       flex: 1,
       child: Container(
           child: Center(child: ScopedModelDescendant<ListModel>(
@@ -82,19 +75,21 @@ class ItemWgt extends StatelessWidget {
               return DragTarget(
                 builder: (cx, accepted, rejected) {
                   return Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Text(i.name,
-                        style: Theme.of(cx).textTheme.headline.copyWith(
-                            color: accepted.isEmpty
-                                ? Colors.black
-                                : Colors.white)));
+                      padding: EdgeInsets.all(16),
+                      child: Text(i.name,
+                          style: Theme.of(cx).textTheme.headline.copyWith(
+                              color: accepted.isEmpty
+                                  ? Colors.black
+                                  : Colors.white)));
                 },
                 onWillAccept: (data) {
                   m.updateSelected(i, dir);
                   HapticFeedback.mediumImpact();
                   return true;
                 },
-                onAccept: (data) {},
+                onAccept: (data) {
+                  m.addToList();
+                },
               );
             },
           )),
@@ -105,6 +100,7 @@ enum Dir { left, right }
 
 class ItemColumn extends StatelessWidget {
   final Dir d;
+
   ItemColumn(this.d);
 
   @override
@@ -121,6 +117,7 @@ class ListModel extends Model {
   Menu menu;
   var _selected, defaultLeft, defaultRight, currentOrder, left, right, currentDir;
   var order = [];
+  var itemsOrdered = [];
 
   void init(mnu) {
     this.menu = mnu;
@@ -130,17 +127,30 @@ class ListModel extends Model {
 
   void updateSelected(selected, dir) {
     this._selected = selected;
-    if (dir == Dir.left) {
-      right = selected.items;
+    order.add(selected.name);
+    var items = selected.items;
+    if (items != null) {
+      if (dir == Dir.left) {
+        right = items;
+      } else {
+        left = items;
+      }
     } else {
-      left = selected.items;
+      // TODO: Show release to add message!
     }
+
     notifyListeners();
   }
+
   List get(dir) => dir == Dir.left ? left : right;
+
   void resetLists() {
     left = defaultLeft;
     right = defaultRight;
     notifyListeners();
+  }
+
+  void addToList() {
+
   }
 }
